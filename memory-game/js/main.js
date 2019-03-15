31a2792ae701491c9ab7dcaddb1f4c22949e4cb1
zack <zack.etc@gmail.com>
2019-03-14 18:13:06 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,19 @@
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
+let cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] == cardsInPlay[1]) {
+        alert("you found a match!")
+    }else{
+        alert("sorry, try again")
+    }
+}
